3ea1db1ef9be8584f5f3311a1e6c863c6d3aec89
Michel Pelletier <pelletier.michel@gmail.com>
2021-08-24 17:19:13 -0700

    first stab at building.

--- tools/install_pg.sh
+++ tools/install_pg.sh
@@ -1,10 +1,8 @@
-yum install -y libreadline-devel zlib-devel
-
 curl -L -O https://ftp.postgresql.org/pub/source/v13.4/postgresql-13.4.tar.gz
 
 tar -xzf postgresql-13.4.tar.gz
 cd postgresql-13.4
-./configure --prefix=/usr/local
+./configure --prefix=/usr/local --without-readline
 make -j 4
 make install
 
