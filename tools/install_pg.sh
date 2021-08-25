3ec2e13a37199d5a2a09495c18f6e347cc5034ec
Michel Pelletier <pelletier.michel@gmail.com>
2021-08-24 22:51:01 -0700

    more wheeling

--- tools/install_pg.sh
+++ tools/install_pg.sh
@@ -1,8 +1,11 @@
+
+yum install -y zlib-devel readline-devel
+
 curl -L -O https://ftp.postgresql.org/pub/source/v13.4/postgresql-13.4.tar.gz
 
 tar -xzf postgresql-13.4.tar.gz
 cd postgresql-13.4
-./configure --prefix=/usr/local --without-readline
+./configure --prefix=/usr/local
 make -j 4
 make install
 
