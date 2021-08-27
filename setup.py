addec9cc74fbaea1fa3b7a45dc0254ba2282b50b
Michel Pelletier <pelletier.michel@gmail.com>
2021-08-27 16:57:11 -0700

    locally building, try for actions.

--- setup.py
+++ setup.py
@@ -1,12 +1,26 @@
 from setuptools import setup
 
+import os
+
+
+def package_files(directory):
+    paths = []
+    for (path, directories, filenames) in os.walk(directory):
+        for filename in filenames:
+            paths.append(os.path.join("..", path, filename))
+    return paths
+
+
+extra_files = package_files("postgresql/binary")
+
 setup(
-    name='postgresql',
-    version='13.4',
-    description='PostgreSQL in a Python Wheel.',
-    author='Michel Pelletier',
-    packages=['postgresql'],    
+    name="postgresql",
+    version="13.4",
+    description="PostgreSQL in a Python Wheel.",
+    author="Michel Pelletier",
+    packages=["postgresql"],
+    package_data={"postgresql": extra_files},
     setup_requires=["cffi>=1.0.0"],
-    cffi_modules=['postgresql/build.py:ffibuilder'],
-    install_requires=["cffi>=1.0.0"],
+    cffi_modules=["postgresql/__init__.py:ffibuilder"],
+    python_requires=">=3.7,<3.10",
 )
--- tools/install_pg.sh
+++ tools/install_pg.sh
@@ -1,16 +1,11 @@
 
 yum install -y zlib-devel readline-devel
-
 curl -L -O https://ftp.postgresql.org/pub/source/v13.4/postgresql-13.4.tar.gz
 
 tar -xzf postgresql-13.4.tar.gz
 cd postgresql-13.4
-./configure --prefix=/usr/local
+./configure --prefix=`pwd`/../postgresql/binary
 make -j 4
 make install
-
 cd ..
-
-which ldconfig && ldconfig || true
-
 
