46f4b9dc95bdf3277909e4041acd6e4b854c739d
Lev Zaplatin <ichi.su.mobile@gmail.com>
2021-12-29 01:13:47 +0300

    Add contrib (#4)
    * feat: add compile contrib
    
    * feat: add checks - create extension: hstore, uuid-ossp, xml2
    
    * fix: minor changes
    
    * fix: use apt-get
    
    * fix: use sudo for apt-get

--- src/postgresql/tests/test_postgresql.py
+++ src/postgresql/tests/test_postgresql.py
@@ -10,3 +10,18 @@ def test_setup_teardown():
 def test_fixture(tmp_postgres):
     pgdata, con_str = tmp_postgres
     postgresql.psql(f'-d "{con_str}" -c "select version()"')
+
+
+def test_default_extension(tmp_postgres):
+    pgdata, con_str = tmp_postgres
+    postgresql.psql(f'-d "{con_str}" -c "CREATE EXTENSION hstore;"')
+
+
+def test_uuid_ossp_extension(tmp_postgres):
+    pgdata, con_str = tmp_postgres
+    postgresql.psql(f'-d "{con_str}" -c \'CREATE EXTENSION "uuid-ossp";\'')
+
+
+def test_xml2_extension(tmp_postgres):
+    pgdata, con_str = tmp_postgres
+    postgresql.psql(f'-d "{con_str}" -c "CREATE EXTENSION xml2;"')
