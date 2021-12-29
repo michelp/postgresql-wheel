e01ec39ddaa33c03dfa7c60ea6b13e7426a30a46
Lev Zaplatin <lev@zaplatin.dev>
2021-12-29 22:03:14 +0300

    fix: remove proj

--- src/tools/install_pg.sh
+++ src/tools/install_pg.sh
@@ -6,7 +6,6 @@ else
     VERSION=13.4
 fi
 JOBS_NUMBER=4
-PROJ_VERSION=8.2.0
 POSTGIS_VERSION=3.2.0
 
 export DEBIAN_FRONTEND=noninteractive
@@ -15,7 +14,8 @@ sudo apt update
 sudo apt install -y zlib1g-dev libreadline-dev libossp-uuid-dev libxml2-dev \
                     libxslt1-dev curl make gcc pkg-config libgeos-dev libxml2-utils \
                     libjson-c-dev proj-bin g++ libsqlite3-dev libtiff-dev \
-                    libcurl4-gnutls-dev libprotobuf-c-dev libgdal-dev libsfcgal-dev
+                    libcurl4-gnutls-dev libprotobuf-c-dev libgdal-dev libsfcgal-dev \
+                    libproj-dev
 
 # Build Postgresql
 curl -L -O https://ftp.postgresql.org/pub/source/v${VERSION}/postgresql-${VERSION}.tar.gz
@@ -26,20 +26,11 @@ make -j ${JOBS_NUMBER} world-bin
 make install-world-bin
 cd ..
 
-# Proj, need for PostGIS
-curl -L -O https://download.osgeo.org/proj/proj-${PROJ_VERSION}.tar.gz
-tar -xzvf proj-${PROJ_VERSION}.tar.gz
-cd proj-${PROJ_VERSION}
-./configure --prefix=`pwd`/../src/proj
-make -j ${JOBS_NUMBER}
-make install
-cd ..
-
-# PostGIS
+# Build PostGIS
 curl -L -O https://download.osgeo.org/postgis/source/postgis-${POSTGIS_VERSION}.tar.gz
 tar -xzvf postgis-${POSTGIS_VERSION}.tar.gz
 cd postgis-${POSTGIS_VERSION}
-./configure --with-pgconfig=`pwd`/../src/postgresql/bin/pg_config --with-projdir=`pwd`/../src/proj
+./configure --with-pgconfig=`pwd`/../src/postgresql/bin/pg_config
 make -j ${JOBS_NUMBER}
 make install
 cd ..
